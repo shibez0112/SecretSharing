72361c446da1356a25d1a37bb51bcfa58773a565
shibez <ginta2888@gmail.com>
2023-05-31 10:32:17 +0700

    add data model

---
+++ SecretSharing.Core/Entities/UserFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SecretSharing.Core.Entities.Identity;
+
+namespace SecretSharing.Core.Entities
+{
+    public class UserFile
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string PublicId { get; set; }
+        public string Url { get; set; }
+        public bool IsAutoDeleted { get; set; }
+
+        [Required]
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
---
+++ SecretSharing.Core/Entities/UserText.cs
@@ -0,0 +1,24 @@
+﻿using SecretSharing.Core.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSharing.Core.Entities
+{
+    public class UserText
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public bool IsAutoDeleted { get; set; }
+
+        [Required]
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
