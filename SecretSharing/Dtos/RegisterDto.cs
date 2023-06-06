77333c434a85b25e00d4a6e3c8846fe834456a24
shibez <ginta2888@gmail.com>
2023-06-06 15:14:33 +0700

    fix: minor changes for password validation.

--- SecretSharing/Dtos/RegisterDto.cs
+++ SecretSharing/Dtos/RegisterDto.cs
@@ -8,8 +8,8 @@ namespace SecretSharing.Dtos
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
-            ErrorMessage = "Passord Must have Atleast 1 Lower 1 Upper and 1 Special Character")]
+        [RegularExpression("(?=^.{6,20}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage = "Passord Must have Atleast 1 Lower 1 Upper and 1 Special Character and also from 6-20 characters")]
         public string Password { get; set; }
     }
 }
